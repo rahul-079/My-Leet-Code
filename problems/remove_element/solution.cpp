0a9b8cb8dd245aedf07d7e1296557ec15078cd52
rahul-079 <121021984+rahul-079@users.noreply.github.com>
2023-02-04 11:07:57 +0000

    Sync LeetCode submission - Remove Element (cpp)

---
+++ problems/remove_element/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int t=0  ;
+        for(int i=0 ;i<nums.size() ;i++){
+            if(nums[i]==val){
+                continue ;
+            }
+            else {
+                nums[t]=nums[i] ;
+                t++ ;
+            }
+
+        }
+        return t ;
+
+    }
+};
